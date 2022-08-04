5e43ee43bd4401585812983afb54cb92c2a63100
Franziska Witzel <franziska.witzel@gmail.com>
2022-08-04 18:55:43 +0200

    counting change using lists

--- Chapter2.rkt
+++ Chapter2.rkt
@@ -410,3 +410,24 @@
     (go nil x))
 
 ; (reverse (list 3 4 5 8 10)) ; (10 8 5 4 3)
+
+;; Counting change from Chapter 1, now using lists
+(define us-coins (list 50 25 10 5 1))
+(define uk-coins (list 100 50 20 10 5 2 1 0.5))
+
+(define (no-more? coin-values)
+  (null? coin-values))
+(define (except-first-denomination coin-values)
+  (cdr coin-values))
+(define (first-denomination coin-values)
+  (car coin-values))
+
+
+(define (cc amount coin-values)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (no-more? coin-values)) 0)
+        (else (+ (cc amount
+                     (except-first-denomination coin-values))
+                 (cc (- amount (first-denomination coin-values)) coin-values)))))
+
+; (cc 100 us-coins) ; 292
