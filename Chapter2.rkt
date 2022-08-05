bc400d0922b7ba09cd8258fdedbfcfbee46995b1
Franziska Witzel <franziska.witzel@gmail.com>
2022-08-05 10:31:31 +0200

    scale list

--- Chapter2.rkt
+++ Chapter2.rkt
@@ -456,4 +456,12 @@
 
 ; (same-parity 1 2 3 4 5 6 7) ; (1 3 5 7)
 ; (same-parity 2 3 4 5 6 7)   ; (2 4 6)
-  
+
+;; Scale every element in a list by a factor -----
+
+(define (scale-list items factor)
+  (if (null? items)
+      nil
+      (cons (* factor (car items)) (scale-list (cdr items) factor))))
+
+; (scale-list (list 1 2 3 4) 0.5)
